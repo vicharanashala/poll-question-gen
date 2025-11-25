608cac11ff7d1a62a2894fbce1c00fa0928bed4b
mamatha12-reddy <mamathareddy3456.reddeppa@gmail.com>
2025-11-25 14:18:47 +0530

    new configuration in both firebase and vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,46 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
+import { comlink } from "vite-plugin-comlink";
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  worker: {
+    format: 'es', 
+    plugins: () => [comlink()]
+  },
   plugins: [react()],
-  assetsInclude: ["**/*.ggml", "**/*.bin", "**/*.wasm"],
-
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
-      "@transcribe/transcriber": resolve(
-        __dirname,
-        "node_modules/@transcribe/transcriber/src/index.js"
-      ),
-      "@transcribe/shout": resolve(
-        __dirname,
-        "node_modules/@transcribe/shout/src/shout/shout.wasm.js"
-      )
-    }
-  },
-
-  optimizeDeps: {
-    exclude: ["@transcribe/transcriber", "@transcribe/shout"]
-  },
-
-  worker: {
-    format: 'es',  // Add this
-    plugins: () => []  // Ensure worker uses same plugins
-  },
-
-  server: {
-    port: 3000,
-    headers: {
-      
     },
-    fs: {
-      allow: [
-        ".",  // Allow project root
-        "public",
-        "node_modules/@transcribe/transcriber",
-        "node_modules/@transcribe/shout",
-      ]
-    }
-  }
+  },
 });
